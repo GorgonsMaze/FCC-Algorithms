c5609fd5672e24f6cdd4f96b2bd57ada0eda4a89
Ian Arsenault <ian_arsenault@protonmail.com>
2017-05-02 00:32:44 -0400

    added Roman Numeral Converter

--- Intermediate/19_Roman-Numeral-Converter.js
+++ Intermediate/19_Roman-Numeral-Converter.js
@@ -16,12 +16,6 @@
 
  **/
 
-// var ones = Math.floor(num % 10),
-//     tens = Math.floor(num / 10 % 10),
-//     hundreds = Math.floor(num / 100 % 10),
-//     thousands = Math.floor(num % 10000 / 1000);
-// 2 , 3
-
 // Method to take number and length of number
 // and assign place values
 // ex: 256 becomes 200, 50, 6
@@ -31,42 +25,44 @@ function getPlacesValue(numToCheck) {
     var placeValueArray = [];
 
     for (var i = 0; i < length; i++) {
-        placeValueArray.push(Math.pow(10, (length- i) -1) * parseInt(digits[i]));
+        placeValueArray.push(Math.pow(10, (length - i) - 1) * parseInt(digits[i]));
     }
+
     return placeValueArray;
 }
 
 function convertToRoman(num) {
-
 
 // I would start with the highest (M if doing roman numerals) and count how many you have, and the remainder.
 //     var year = 1979;
 //     var howManyMs = Math.floor(year / 1000);
 //     var remainder = year % 1000;
 
-    const romanNumeralObj = {
-        1: "I",
-        5: "V",
-        10: "X",
-        50: "L",
-        100: "C",
-        500: "D",
-        1000: "M"
+    var romanNumeralArray = [];
+    var romanNumeralObj = {
+        1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII",
+        9: "IX", 10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX",
+        80: "LXXX", 90: "XC", 100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC",
+        800: "DCCC", 900: "CM", 1000: "M", 2000: "MM", 3000: "MMM"
     };
 
-    return getPlacesValue(num);
+
+    var placeValue = getPlacesValue(num);
+
+    for (var i = 0; i < placeValue.length; i++) {
+        for (var key in romanNumeralObj) {
+            if (romanNumeralObj.hasOwnProperty(key)) {
+                if (placeValue[i] == key) {
+                    romanNumeralArray.push(romanNumeralObj[key]);
+                }
+            }
+        }
+    }
+
+    return romanNumeralArray.join('');
+
 
 }
 
 
-console.log(convertToRoman(342));
-
-
-//
-// const romanNumeralObj = {
-//     1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII",
-//     9: "IX", 10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX",
-//     80: "LXXX", 90: "XC", 100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC",
-//     800: "DCCC", 900: "CM", 1000: "M"
-// };
-
+console.log(convertToRoman(3999));
