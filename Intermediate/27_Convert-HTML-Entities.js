3a70b5854c9762426ab3db9ca9592e6d0f2e27be
Ian Arsenault <ian_arsenault@protonmail.com>
2017-06-07 22:45:33 -0400

    completed HTML entitie converter

--- Intermediate/27_Convert-HTML-Entities.js
+++ Intermediate/27_Convert-HTML-Entities.js
@@ -9,16 +9,13 @@
  */
 
 function convertHTML(str) {
-    var regex = /[<|>|"|'|&]/g;
-
-    var tempString = str.split('');
-
-    while (match = regex.exec(str)) {
-        console.log(match.index + ' ' + str.lastIndex);
-    }
-    return tempString;
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
 }
 
-console.log(convertHTML("Dolce & Gabbana"));
-
-console.log(convertHTML('Stuff in "quotation marks"'));
+console.log(convertHTML("Dolce & Gabbana")); //should return Dolce &​amp; Gabbana.
+console.log(convertHTML("Hamburgers < Pizza < Tacos")); //should return Hamburgers &​lt; Pizza &​lt; Tacos.
+console.log(convertHTML("Sixty > twelve")); //should return Sixty &​gt; twelve.
+console.log(convertHTML('Stuff in "quotation marks"')); //should return Stuff in &​quot;quotation marks&​quot;.
+console.log(convertHTML("Shindler's List")); //should return Shindler&​apos;s List.
+console.log(convertHTML("<>")); //should return &​lt;&​gt;.
+console.log(convertHTML("abc")); //should return abc.
